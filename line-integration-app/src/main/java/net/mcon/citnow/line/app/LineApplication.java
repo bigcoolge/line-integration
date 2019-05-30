338c86893f8ef875742cfa1b7404c5546151df6a
Tom Zhang <tom.zhang@mcon-group.com>
2019-05-30 14:55:41 +0800

    add push message

--- line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
@@ -1,18 +1,29 @@
 package net.mcon.citnow.line.app;
 
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
 @LineMessageHandler
+@RestController
 public class LineApplication {
 
 	public static void main(String[] args) {
@@ -30,5 +41,19 @@ public class LineApplication {
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
+    }
+    
+    @Autowired
+    LineMessagingClient client;
+    
+    @GetMapping("/api/push")
+    public String pushMessageToTester() throws InterruptedException, ExecutionException {
+    	System.out.println("send out text message to Tom!");
+    	PushMessage msg = new PushMessage("U55dbae93eeaee85433eeb60d77461e0b", Arrays.asList(new TextMessage("Hello, Tom")));
+    	CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
+    	if(response != null) {
+    		return response.get().getMessage();
+    	}
+    	return "why I am here?";
     }
 }
