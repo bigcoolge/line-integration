762e62806e5076de8d24c05560448c968e4d918a
Tom Zhang <tom.zhang@mcon-group.com>
2019-05-31 15:00:33 +0800

    add api to get user's phone etc

--- line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linecorp.bot.client.LineMessagingClient;
@@ -30,30 +32,36 @@ public class LineApplication {
 		SpringApplication.run(LineApplication.class, args);
 	}
 
-    @EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        System.out.println("source: " + event.getSource().getUserId() + " | " + event.getSource().getClass());
-        final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
-    }
+	@EventMapping
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		System.out.println("source: " + event.getSource().getUserId() + " | " + event.getSource().getClass());
+		final String originalMessageText = event.getMessage().getText();
+		return new TextMessage(originalMessageText);
+	}
 
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
-    }
-    
-    @Autowired
-    LineMessagingClient client;
-    
-    @GetMapping("/api/push")
-    public String pushMessageToTester() throws InterruptedException, ExecutionException {
-    	System.out.println("send out text message to Tom!");
-    	PushMessage msg = new PushMessage("U55dbae93eeaee85433eeb60d77461e0b", Arrays.asList(new TextMessage("Hello, Tom")));
-    	CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
-    	if(response != null) {
-    		return response.get().getMessage();
-    	}
-    	return "why I am here?";
-    }
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+	}
+
+	@Autowired
+	LineMessagingClient client;
+
+	@GetMapping("/api/push")
+	public String pushMessageToTester(@RequestParam String userId, @RequestParam String name,
+			@RequestParam String phone) throws InterruptedException, ExecutionException {
+		System.out.println(" ===== " + userId + " | " + name + " | " + phone);
+		if(StringUtils.isAnyBlank(userId, name, phone)) {
+			System.out.println("parameters are not correct!");
+			return "FAILURE";
+		}
+		System.out.println("send out text message to " + name);
+		PushMessage msg = new PushMessage(userId, Arrays.asList(new TextMessage("Thank you, " + name  + " !")));
+		CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
+		if (response != null) {
+			return response.get().getMessage();
+		}
+		return "why I am here?";
+	}
 }
