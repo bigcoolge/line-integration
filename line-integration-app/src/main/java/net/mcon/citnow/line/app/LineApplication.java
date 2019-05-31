b1d8136359823a2f817878644c9c1b1b54b35dbe
Tom Zhang <tom.zhang@mcon-group.com>
2019-05-31 16:54:55 +0800

    send image message

--- line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
@@ -1,6 +1,8 @@
 package net.mcon.citnow.line.app;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +21,14 @@ import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import net.mcon.citnow.line.app.model.VideoInfo;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -52,21 +59,37 @@ public class LineApplication {
 	public String pushMessageToTester(@RequestParam String userId, @RequestParam String name,
 			@RequestParam String phone) throws InterruptedException, ExecutionException {
 		System.out.println(" ===== " + userId + " | " + name + " | " + phone);
-		if(StringUtils.isAnyBlank(userId, name, phone)) {
+		if (StringUtils.isAnyBlank(userId, name, phone)) {
 			System.out.println("parameters are not correct!");
 			return "FAILURE";
 		}
 		System.out.println("send out text message to " + name);
-		PushMessage msg = new PushMessage(userId, Arrays.asList(new TextMessage("Thank you, " + name  + " !")));
+		PushMessage msg = new PushMessage(userId, Arrays.asList(new TextMessage("Thank you, " + name + " !")));
 		CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
 		if (response != null) {
 			return response.get().getMessage();
 		}
 		return "why I am here?";
 	}
-	
-	@GetMapping("/api/send")
-	public boolean sendNotification() {
+
+	static Map<String, String> userMap = new HashMap<>();
+	{
+		userMap.put("13520346656", "U55dbae93eeaee85433eeb60d77461e0b");
+	}
+
+	@PostMapping("/api/send")
+	public boolean sendNotification(@RequestBody VideoInfo videoInfo) throws InterruptedException, ExecutionException {
+		if (videoInfo != null && !StringUtils.isAnyBlank(videoInfo.getChannelId(), videoInfo.getUserPhone(),
+				videoInfo.getVideoUrl(), videoInfo.getPicUrl())) {
+			System.out.println(" ===== " + videoInfo);
+			//TODO: get userId by Phone
+			PushMessage msg = new PushMessage("U55dbae93eeaee85433eeb60d77461e0b",
+					Arrays.asList(new ImageMessage(videoInfo.getVideoUrl(), videoInfo.getPicUrl())));
+			CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
+			if (response != null) {
+				return StringUtils.isNotBlank(response.get().getMessage());
+			}
+		}
 		return false;
 	}
 }
---
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/model/VideoInfo.java
@@ -0,0 +1,11 @@
+package net.mcon.citnow.line.app.model;
+
+import lombok.Data;
+
+@Data
+public class VideoInfo {
+	private String channelId;
+	private String userPhone;
+	private String videoUrl;
+	private String picUrl;
+}
