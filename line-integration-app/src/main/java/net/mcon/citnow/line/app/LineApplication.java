f6a2e39633da510b1aa2b021ae66bf8f7788d36c
Tom Zhang <tom.zhang@mcon-group.com>
2019-06-03 16:46:12 +0800

    add more information in video

--- line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/LineApplication.java
@@ -1,6 +1,5 @@
 package net.mcon.citnow.line.app;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -23,14 +22,8 @@ import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.ImagemapMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.message.imagemap.ImagemapArea;
-import com.linecorp.bot.model.message.imagemap.ImagemapBaseSize;
-import com.linecorp.bot.model.message.imagemap.ImagemapExternalLink;
-import com.linecorp.bot.model.message.imagemap.ImagemapVideo;
-import com.linecorp.bot.model.message.imagemap.URIImagemapAction;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -88,18 +81,20 @@ public class LineApplication {
 	public boolean sendNotification(@RequestBody VideoInfo videoInfo)
 			throws InterruptedException, ExecutionException, URISyntaxException {
 		if (videoInfo != null && !StringUtils.isAnyBlank(videoInfo.getChannelId(), videoInfo.getUserPhone(),
-				videoInfo.getVideoUrl(), videoInfo.getPicUrl())) {
+				videoInfo.getSummary(), videoInfo.getDescription(), videoInfo.getVideoUrl(), videoInfo.getPicUrl())) {
 			System.out.println(" ===== " + videoInfo);
 			// TODO: get userId by Phone
 			TextMessage content = new TextMessage("The video is ready: " + videoInfo.getVideoUrl());
-//			ImagemapMessage content = ImagemapMessage.builder()
-//					.baseUrl(videoInfo.getPicUrl())
-//					.altText("Your car's video is ready")
-//					.baseSize(new ImagemapBaseSize(1040, 1040))
-//					.actions(Arrays.asList(new URIImagemapAction(videoInfo.getVideoUrl(), new ImagemapArea(0, 0, 700, 700))))
-//					.video(new ImagemapVideo(new URI(videoInfo.getVideoUrl()), new URI(videoInfo.getPicUrl()), new ImagemapArea(0, 0, 700, 700),
-//							new ImagemapExternalLink(new URI(videoInfo.getVideoUrl()), "CitNOW video")))
-//					.build();
+			// ImagemapMessage content = ImagemapMessage.builder()
+			// .baseUrl(videoInfo.getPicUrl())
+			// .altText("Your car's video is ready")
+			// .baseSize(new ImagemapBaseSize(1040, 1040))
+			// .actions(Arrays.asList(new URIImagemapAction(videoInfo.getVideoUrl(), new
+			// ImagemapArea(0, 0, 700, 700))))
+			// .video(new ImagemapVideo(new URI(videoInfo.getVideoUrl()), new
+			// URI(videoInfo.getPicUrl()), new ImagemapArea(0, 0, 700, 700),
+			// new ImagemapExternalLink(new URI(videoInfo.getVideoUrl()), "CitNOW video")))
+			// .build();
 			PushMessage msg = new PushMessage("U55dbae93eeaee85433eeb60d77461e0b", Arrays.asList(content));
 			CompletableFuture<BotApiResponse> response = client.pushMessage(msg);
 			if (response != null) {
--- line-integration-app/src/main/java/net/mcon/citnow/line/app/model/VideoInfo.java
+++ line-integration-app/src/main/java/net/mcon/citnow/line/app/model/VideoInfo.java
@@ -6,6 +6,15 @@ import lombok.Data;
 public class VideoInfo {
 	private String channelId;
 	private String userPhone;
+
 	private String videoUrl;
 	private String picUrl;
+	private VideoType type;
+	private String summary;
+	private String description;
+
+
+	public static enum VideoType {
+		SALES, AFTERSALES;
+	}
 }
